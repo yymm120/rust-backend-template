1c0fccfd5c5379f8637146ffdd44e8d1c9f6e832
xinhao <1208401731@qq.com>
2024-02-01 19:58:24 +0800

    add database connection

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -5,7 +5,10 @@ async fn hello_world() -> &'static str {
 }
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
+async fn main(
+    #[shuttle_shared_db::Postgres] pool: sqlx::PgPool,
+    // #[shuttle_secrets::Secrets] secrets: shuttle_secrets::SecretStore,
+) -> shuttle_axum::ShuttleAxum {
     let router = Router::new().route("/", get(hello_world));
 
     Ok(router.into())
